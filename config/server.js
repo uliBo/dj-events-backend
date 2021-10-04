753a733f43ad1326c5883cba1de55f25b7899aab
Ulrich Bosbach <ulrich.bosbach@eskumed.de>
2021-10-04 22:01:58 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ebaf2607cc59f6fa206fdd284bd1938c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '44348c98-9a00-4354-b436-a5686da74794'
+};
